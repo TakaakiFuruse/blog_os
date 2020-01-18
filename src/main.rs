58d90575a368122a51a1bf62163a06f0dab50bf2
TakaakiFuruse <takaakifuruse@gmail.com>
2020-01-18 14:15:55 +0900

    finished Minimal Rust Kernel

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,17 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"HELOO WORLDY!!";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    loop {}
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate(){
+        unsafe{
+            *vga_buffer.offset(i as isize * 2 ) = byte;
+            *vga_buffer.offset(i as isize * 2 +1 ) = 0xb;
+        }
+    }
+    loop{}
 }
